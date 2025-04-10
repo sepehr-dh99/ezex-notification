828327ed852ec3f12fea393553cf490775583a89
sepehr-dh99 <sepehr.dh99@gmail.com>
2025-04-10 10:46:30 +0330

    fix: log levels

--- internal/cmd/server/main.go
+++ internal/cmd/server/main.go
@@ -3,12 +3,12 @@ package main
 
 import (
 	"flag"
-	"log"
 
 	grpcserver "github.com/ezex-io/ezex-notification/api/grpc"
 	"github.com/ezex-io/ezex-notification/internal/adapters/smtp"
 	"github.com/ezex-io/ezex-notification/internal/config"
 	"github.com/ezex-io/ezex-notification/internal/interactors"
+	"github.com/ezex-io/gopkg/logger"
 	"github.com/joho/godotenv"
 )
 
@@ -16,17 +16,19 @@ func main() {
 	envFile := flag.String("env", ".env", "Path to environment file")
 	flag.Parse()
 
+	logging := logger.NewSlog(nil)
+
 	// Load the specified env file
 	if err := godotenv.Load(*envFile); err != nil {
-		log.Printf("Failed to load env file '%s': %v. Continuing with system environment...", *envFile, err)
+		logging.Warn("Failed to load env file '%s': %v. Continuing with system environment...", *envFile, err)
 	} else {
-		log.Printf("Loaded environment variables from '%s'", *envFile)
+		logging.Debug("Loaded environment variables from '%s'", *envFile)
 	}
 
 	// Load config from environment
-	cfg, err := config.Load()
-	if err != nil {
-		log.Fatalf("failed to load config: %v", err)
+	cfg := config.Load()
+	if err := cfg.BasicCheck(); err != nil {
+		logging.Fatal("failed to load config: %v", err)
 	}
 
 	emailSender := smtp.NewSMTPAdapter(cfg.SMTP)
@@ -38,13 +40,13 @@ func main() {
 		Port: cfg.GRPC.Port,
 	})
 	if err != nil {
-		log.Fatalf("failed to create gRPC server: %v", err)
+		logging.Fatal("failed to create gRPC server: %v", err)
 	}
 
 	go server.Start()
-	log.Printf("Starting gRPC server on port %s", cfg.GRPC.Port)
+	logging.Debug("Starting gRPC server on port %s", cfg.GRPC.Port)
 
 	if err := <-server.Notify(); err != nil {
-		log.Fatalf("gRPC server error: %v", err)
+		logging.Fatal("gRPC server error: %v", err)
 	}
 }
